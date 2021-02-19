443b0921a58f6eff61a87c689e467c3b4909956f
skruud <skruud@gmail.com>
2021-02-20 00:05:35 +0100

    Update sql_database.py

--- sql_database.py
+++ sql_database.py
@@ -4,7 +4,7 @@ from config import config
 class SQLDatabase:
 
     def __init__(self):
-        connect()
+        self.connect()
 
     def connect(self):
         self.conn = None
@@ -15,7 +15,7 @@ class SQLDatabase:
             # connect to the PostgreSQL database
             self.conn = psycopg2.connect(**params)
             # create a new cursor
-            self.cur = conn.cursor()
+            self.cur = self.conn.cursor()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
             self.cur.close()
@@ -31,20 +31,20 @@ class SQLDatabase:
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
-            if conn is not None:
+            if self.conn is not None:
                 self.conn.close()
 
     def insert_data(self, table, columns, values):
         """ insert a new vendor into the vendors table """
-        sql = """INSERT INTO {table}({columns})
-                VALUES({values}) RETURNING vendor_id;""".format(table, columns, values)
+        sql = """INSERT INTO %s(%s)
+                VALUES(%s);""" %(table, columns, values)
         
         try:
             # execute the INSERT statement
-            self.cur.execute(sql, (vendor_name,))
+            self.cur.execute(sql)
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
             self.cur.close()
-            if conn is not None:
+            if self.conn is not None:
                 self.conn.close()
         
