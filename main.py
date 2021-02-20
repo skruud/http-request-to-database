e7333cb710c3aadb17e43bd5901fa708b8c74a5c
skruud <skruud@gmail.com>
2021-02-20 22:28:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,40 @@ import requests
 import json
 from sql_database import SQLDatabase
 
-
-
-sql = """INSERT INTO vendors(vendor_name)
-         VALUES(%s) RETURNING vendor_id;"""
-#insert_vendor(sql)
-
 data_link = 'https://8syg62n83a.execute-api.eu-north-1.amazonaws.com/dev/vd'
 
 x = requests.get(data_link)
 data = x.json()
-print(data[0]['sector'])
 
+tables = ['sector', 'occupation', 
+          'form', 'industry', 'role', ]
 
+#Correct displaced items
+sector_categories = ['Franchise/Selvstendig næringsdrivende', 
+                     'Offentlig', 'Privat', 'Samvirke', 'Organisasjoner']
+form_categories = ['Heltid', 'Deltid']
+role_categories = ['Leder', 'Direktør', 'Fagleder',]
 
 database = SQLDatabase()
 
 for date_and_location in data:
-    for sector in date_and_location['sector']:
-        amount = date_and_location['sector'][sector]
-        date = date_and_location['date'][0:10]
-        location = date_and_location['location']
+    for table in tables:
+        for element in date_and_location[table]:
+            amount = date_and_location[table][element]
+            date = date_and_location['date'][0:10]
+            location = date_and_location['location']
 
-        table = "public.sector"
-        columns = "sector, amount, date, location"
-        values = "'%s', %s, '%s', '%s'" %(sector, amount, date, location)
+            public_table = "public.%s" %(table)
+            if element in sector_categories:
+                public_table = "public.sector" 
+            if element in form_categories:
+                public_table = "public.form" 
+            if element in role_categories:
+                public_table = "public.role" 
+            columns = "category, amount, date, location"
+            values = "'%s', %s, '%s', '%s'" %(element, amount, date, location)
 
-        database.insert_data(table, columns, values)
+            database.insert_data(public_table, columns, values)
 
 database.disconnect()
 
